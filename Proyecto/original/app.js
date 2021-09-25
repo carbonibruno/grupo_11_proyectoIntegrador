b7da0f133ecb71d1cda8e8bbeeae184c6e3a1717
unknown <msalperin@hotmail.com>
2021-09-25 11:20:23 -0300

    elimine carpeta b de proyecto

--- Proyecto/original/app.js
+++ Proyecto/original/app.js
@@ -5,9 +5,23 @@ const path = require ("path");
 const publicPath = path.resolve(__dirname, "./public");
 app.use (express.static(publicPath));
 
-app.listen(5000, () => {
-    console.log("App listening on port http://localhost:5000/");
+app.listen(4000, () => {
+    console.log("App listening on port http://localhost:4000/");
 });
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"));
+})
+app.get("/producto", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/producto.html"));
+})
+app.get("/carrito", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/carrito.html"));
+})
+
+app.get("/acceso", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/acceso.html"));
+})
+
+app.get("/newuser", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/newuser.html"));
 })
