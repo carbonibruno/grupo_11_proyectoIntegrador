d33f8493d21c8099e57ff89d32e6d0362b0623f3
unknown <msalperin@hotmail.com>
2021-09-27 16:29:40 -0300

    cambios

--- Proyecto/original/src/routes/usersRouter.js
+++ Proyecto/original/src/routes/usersRouter.js
@@ -7,4 +7,5 @@ const usuariosController = require('../../controllers/usersController');
 routerUsuarios.get('/acceso', usuariosController.acceso);
 routerUsuarios.get('/acceso/nuevoUsuario', usuariosController.newUser);
 
+
 module.exports = routerUsuarios;
