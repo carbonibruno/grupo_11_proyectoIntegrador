712a7a573ce55fa3f2fae401a40563b735182bb8
unknown <msalperin@hotmail.com>
2021-09-28 15:02:05 -0300

    ciclo creacion de producto ok

--- Proyecto/original/controllers/productController.js
+++ Proyecto/original/controllers/productController.js
@@ -1,4 +1,10 @@
 
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/baseDeDatosProductos.json');
+const productos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const productController = {
     
     indexProductos: (req,res) => {
@@ -12,9 +18,37 @@ const productController = {
     carrito: (req,res) => {
     res.render("carrito")
     },
-
+    
+    /*crear producto*/
     crear: (req,res) => {
     res.render('crearProducto');    
+    },
+    
+    /*guardar producto creado*/
+    store: (req,res) => {
+        
+        const newProduct = {
+            id: productos[products.length - 1].id + 1,
+            name: req.body.nombre,
+            price: req.body.price,
+            discount: req.body.discount,
+            category: req.body.category,
+            description: req.body.description,
+            image: "default-image.png",
+         }
+ 
+         productos.push(newProduct);
+  
+         fs.writeFileSync(productsFilePath, JSON.stringify(productos, null, " "));
+ 
+         res.redirect("/crearProducto"); /*cambiar cuando este vista productos */
+        
+    },
+    
+    /*editar producto existente*/
+    
+    editar: (req,res) => {
+    res.render('editarProducto');    
     }
 
      
--- Proyecto/original/src/routes/productRouter.js
+++ Proyecto/original/src/routes/productRouter.js
@@ -6,7 +6,18 @@ const productController = require('../../controllers/productController');
 routerProductos.get('/', productController.indexProductos);
 routerProductos.get('/detalle', productController.detalle);
 routerProductos.get('/carrito', productController.carrito);
-routerProductos.get('/crear', productController.crear);
 
+routerProductos.get('/crear', productController.crear);
+routerProductos.post('/', productController.store); 
+
+
+
+routerProductos.get('/editar/:id', productController.editar);
+
+
+/* editar
+router.get('/edit/:id', productsController.edit); 
+router.put('/edit/:id', productsController.update); 
+*/
 
 module.exports = routerProductos;
