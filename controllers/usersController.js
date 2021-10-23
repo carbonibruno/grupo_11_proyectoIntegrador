03b91b2fa702218f6708c19da222623f1a5aecf5
unknown <msalperin@hotmail.com>
2021-10-23 13:52:22 -0300

    hasta usuario puede ingresar ok

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -11,6 +11,14 @@ const usersController = {
 
     processRegister: (req,res) =>{
     
+    let userInDB = modeloUsuario.findByField('email', req.body.email);
+    
+    /*aca va la validacion del mail para no repertir usuario minuto49
+    if (userInDB){
+        
+    }
+    */
+    
     let userToCreate = {
         ...req.body,
         password: bcryptjs.hashSync(req.body.password, 10)
@@ -19,7 +27,7 @@ const usersController = {
 
     modeloUsuario.create(userToCreate);    
     
-    res.send('Usuario Agregado');
+    res.redirect('/usuarios/acceso');
     
     }, 
 
@@ -27,8 +35,31 @@ const usersController = {
         res.render("nuevoUsuario");
     },
 
-    
-    
+    loginProcess: (req,res) => {
+       
+       let userToLogin = modeloUsuario.findByField('email', req.body.email); 
+       
+       if(userToLogin){
+         let isOkThePassword = bcryptjs.compareSync(req.body.password, userToLogin.password);
+         if (isOkThePassword){
+             return res.send('Okey')
+         }  
+         return res.send(userToLogin);
+       }
+       
+       /*aca va validacion error*/
+       /* minuto 59 return res.render('nuevoUsuario', {
+           errors: {
+               email: {
+                   msg: 'No se encuentra este email en nuestra base de datos'
+                  }
+               }
+           }); */    
+
+       /*res.render('nuevoUsuario');*/
+    }
+
+       
 }
 
 
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,8 +3,11 @@ const routerUsuarios = express.Router();
 
 const usuariosController = require('../controllers/usersController');
 
+/*acceso login usuario*/
 routerUsuarios.get('/acceso', usuariosController.acceso);
+routerUsuarios.post('/acceso', usuariosController.loginProcess);
 
+/*acceso nuevo usuario*/
 routerUsuarios.get('/acceso/nuevoUsuario', usuariosController.nuevoUsuario);
 routerUsuarios.post('/acceso/nuevoUsuario', usuariosController.processRegister);
 
