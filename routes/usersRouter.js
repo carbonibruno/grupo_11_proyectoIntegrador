2b82b732df4e5986e8f73236983ae8c4fe848b31
unknown <msalperin@hotmail.com>
2021-12-01 14:07:56 -0300

    adelanto validacion backend

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,9 +2,19 @@ const express = require('express');
 const routerUsuarios = express.Router();
 const guestMiddleware = require('../middlewares/guestMiddleware')
 
+
 const usuariosController = require('../controllers/usersController');
 const router = require('./mainRouter');
 const usersController = require('../controllers/usersController');
+
+const { body }  = require('express-validator');
+
+const validations = [
+    body("first_name").notEmpty().withMessage('Ingrese su nombre'),
+    body('last_name').notEmpty().withMessage('Ingrese su apellido'),
+    body('email').notEmpty().withMessage('Ingrese un mail valido'),
+    body('password').notEmpty().withMessage('El minimo del password son 8 caracteres'),
+]
 
 /*acceso login usuario*/
 routerUsuarios.get('/acceso', guestMiddleware, usuariosController.acceso);
@@ -12,9 +22,10 @@ routerUsuarios.post('/acceso', usuariosController.loginProcess);
 
 /*acceso nuevo usuario*/
 routerUsuarios.get('/acceso/nuevoUsuario',guestMiddleware, usuariosController.nuevoUsuario);
-routerUsuarios.post('/acceso/nuevoUsuario', usuariosController.processRegister);
+routerUsuarios.post('/acceso/nuevoUsuario', validations , usuariosController.processRegister);
 
 /*acceso perfil usuario*/
+
 routerUsuarios.get('/acceso/perfil', usuariosController.perfil);
 
 /*logout*/
