dcefbed0c30ed2c9abab4d28f262fb3126060ad2
unknown <msalperin@hotmail.com>
2022-01-03 19:13:25 -0300

    avances

--- middlewares/userLoggedMiddleware.js
+++ middlewares/userLoggedMiddleware.js
@@ -1,6 +1,6 @@
 const modeloUsuario = (require('../models/modeloUsuario'))
 
-
+const db = require ("../database/models");
 
 function userLoggedMiddleware(req,res,next){
     
@@ -8,8 +8,42 @@ function userLoggedMiddleware(req,res,next){
 
     let emailInCookie = req.cookies.userEmail;
 
-    let userFromCookie = modeloUsuario.findByField('email', emailInCookie);
+    db.Users.findOne({
+        where: {
+            email: emailInCookie
+        },
+    })
+    .then((resultado) => {
 
+        let userFromCookie = resultado;
+
+        if(userFromCookie){
+            req.session.userLogged = userFromCookie;
+        }
+
+        if(req.session && req.session.userLogged){
+            res.locals.isLogged = true;
+            res.locals.userLogged = req.session.userLogged;
+        }
+    })
+    .catch((error) => console.log(error))
+
+
+    next();
+
+
+}
+
+module.exports = userLoggedMiddleware;
+
+
+
+/* res.locals.isLogged = false;
+
+    let emailInCookie = req.cookies.userEmail;
+
+    let userFromCookie = modeloUsuario.findByField('email', emailInCookie);
+    
     if(userFromCookie){
         req.session.userLogged = userFromCookie;
     }
@@ -19,9 +53,6 @@ function userLoggedMiddleware(req,res,next){
         res.locals.userLogged = req.session.userLogged;
     }
 
-    next();
+    next(); */
 
 
-}
-
-module.exports = userLoggedMiddleware;
